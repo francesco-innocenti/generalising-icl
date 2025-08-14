38abb9066ef53b65158465f23365f0f1232e172f
Francesco Innocenti <F.Innocenti@sussex.ac.uk>
2025-08-14 15:48:42 +0100

    update plotting

--- plotting.py
+++ plotting.py
@@ -1,6 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",
+    "font.serif": ["Computer Modern Roman"], # Or "Times New Roman"
+    "axes.formatter.use_mathtext": True,
+})
+
 
 def plot_ΔWs_alignments(ΔWs_alignments, save_path):
     fig, ax = plt.subplots(figsize=(5, 4), dpi=150)
@@ -13,12 +20,9 @@ def plot_ΔWs_alignments(ΔWs_alignments, save_path):
     )
     cbar = fig.colorbar(im)
     cbar.set_ticks([1, 0, -1])
-    cbar.ax.tick_params(labelsize=16)
-    cbar.set_label(
-        "Directional alignment", 
-        fontsize=16, 
-        labelpad=10
-    )
+    cbar.ax.tick_params(labelsize=18)
+    cbar.set_label("Directional alignment", fontsize=18)
+    
     ax.set_title("$t = 0$", fontsize=18, pad=10)
     ax.set_ylabel("$\Delta W_i(C)$", fontsize=18)
     ax.set_xlabel("$\Delta W_j(C)$", fontsize=18)
@@ -29,7 +33,7 @@ def plot_ΔWs_alignments(ΔWs_alignments, save_path):
         direction="in", 
         length=6, 
         width=1, 
-        labelsize=14
+        labelsize=16
     )
     ticks = np.array([0, 50])
     plt.xticks(ticks, ticks + 1)
